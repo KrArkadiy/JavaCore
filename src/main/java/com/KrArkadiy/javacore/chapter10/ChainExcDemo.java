55bafba497ea48c8b48af1052d24316e6c423899
arkadiy krylosov <arkady.krylosov@yandex.ru>
2021-06-16 01:14:38 +0300

    Chapter 10 added

---
+++ src/main/java/com/KrArkadiy/javacore/chapter10/ChainExcDemo.java
@@ -0,0 +1,25 @@
+package main.java.com.KrArkadiy.javacore.chapter10;
+
+public class ChainExcDemo {
+    static void demoproc(){
+
+        //создать исключение
+        NullPointerException e = new NullPointerException("верхний уровень");
+
+        //добавить причину исключения
+        e.initCause(new ArithmeticException("причина"));
+        throw e;
+    }
+
+    public static void main(String[] args) {
+        try{
+            demoproc();
+        } catch (NullPointerException e){
+            //вывести исключение верхнего уровеня
+            System.out.println("Перехвачено исключение: " + e);
+
+            //вывести исключение, послужившее причиной для исключения верхнего уровня
+            System.out.println("Первопричина: " + e.getCause());
+        }
+    }
+}
